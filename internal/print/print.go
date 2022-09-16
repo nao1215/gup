77f8747b4738ae473aee4032a838a57f7a9c1802
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-16 22:34:15 +0900

    Add unit test for print package

--- internal/print/print.go
+++ internal/print/print.go
@@ -1,3 +1,4 @@
+// Package print defines functions to accept colored standard output and user input
 package print
 
 import (
@@ -17,30 +18,40 @@ var (
 	Stderr = colorable.NewColorableStderr()
 )
 
-// Info print information message at STDOUT.
+// Info print information message at STDOUT in green.
+// This function is used to print some information (that is not error) to the user.
 func Info(msg string) {
 	fmt.Fprintf(Stdout, "%s:%s: %s\n",
 		cmdinfo.Name, color.GreenString("INFO "), msg)
 }
 
-// Warn print warning message at STDERR.
+// Warn print warning message at STDERR in yellow.
+// This function is used to print warning message to the user.
 func Warn(err interface{}) {
 	fmt.Fprintf(Stderr, "%s:%s: %v\n",
 		cmdinfo.Name, color.YellowString("WARN "), err)
 }
 
-// Err print error message at STDERR.
+// Err print error message at STDERR in yellow.
+// This function is used to print error message to the user.
 func Err(err interface{}) {
 	fmt.Fprintf(Stderr, "%s:%s: %v\n",
 		cmdinfo.Name, color.HiYellowString("ERROR"), err)
 }
 
-// Fatal print dying message at STDERR.
+// OsExit is wrapper for  os.Exit(). It's for unit test.
+var OsExit = os.Exit
+
+// Fatal print dying message at STDERR in red.
+// After print message, process will exit
 func Fatal(err interface{}) {
 	fmt.Fprintf(Stderr, "%s:%s: %v\n",
 		cmdinfo.Name, color.RedString("FATAL"), err)
-	os.Exit(1)
+	OsExit(1)
 }
+
+// FmtScanln is wrapper for fmt.Scanln(). It's for unit test.
+var FmtScanln = fmt.Scanln
 
 // Question displays the question in the terminal and receives an answer from the user.
 func Question(ask string) bool {
@@ -48,7 +59,7 @@ func Question(ask string) bool {
 
 	fmt.Fprintf(Stdout, "%s:%s: %s",
 		cmdinfo.Name, color.GreenString("CHECK"), ask+" [Y/n] ")
-	_, err := fmt.Scanln(&response)
+	_, err := FmtScanln(&response)
 	if err != nil {
 		// If user input only enter.
 		if strings.Contains(err.Error(), "expected newline") {
