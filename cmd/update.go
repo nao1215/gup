0f5ae372a5b5906b241a208523fca646c3cd3882
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-19 01:39:57 +0900

    OsExit --> os.Exit()

--- cmd/update.go
+++ cmd/update.go
@@ -142,7 +142,7 @@ func catchSignal(c chan os.Signal, dryRunManager *goutil.GoPaths) {
 			if err := dryRunManager.EndDryRunMode(); err != nil {
 				print.Err(fmt.Errorf("can not change dry run mode to normal mode: %w", err))
 			}
-			OsExit(0)
+			os.Exit(0)
 		default:
 			time.Sleep(1 * time.Second)
 		}
